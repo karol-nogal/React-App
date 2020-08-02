9762a342f274ae727ce3545a3d2a833c4338abea
karol-nogal <karolnogal92@gmail.com>
2020-08-02 14:24:57 +0200

    Updates

---
+++ precache-manifest.1b18914120f66a234c90cc5472e4108e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1655aff515ca72afbeac4061600f153",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "3bb918e15f12e9649074",
+    "url": "/React-App/static/css/2.2643f009.chunk.css"
+  },
+  {
+    "revision": "cf879004c781cfd9ce9d",
+    "url": "/React-App/static/css/main.85217fed.chunk.css"
+  },
+  {
+    "revision": "3bb918e15f12e9649074",
+    "url": "/React-App/static/js/2.7d148d64.chunk.js"
+  },
+  {
+    "revision": "cf879004c781cfd9ce9d",
+    "url": "/React-App/static/js/main.3e272f91.chunk.js"
+  },
+  {
+    "revision": "d1b85ddc0457d7a1843e",
+    "url": "/React-App/static/js/runtime-main.2a89a368.js"
+  },
+  {
+    "revision": "b2d0a21aadb43556e775ac9b95552c73",
+    "url": "/React-App/static/media/friends.b2d0a21a.png"
+  },
+  {
+    "revision": "d9944d040ad4e901a9b55ffd5c4ca103",
+    "url": "/React-App/static/media/join.d9944d04.png"
+  },
+  {
+    "revision": "00f4e123b73ae425afc83b316c17cdbe",
+    "url": "/React-App/static/media/open.00f4e123.png"
+  },
+  {
+    "revision": "5c30946ca1cdc766a3833920a6a715f9",
+    "url": "/React-App/static/media/spot.5c30946c.png"
+  }
+]);
